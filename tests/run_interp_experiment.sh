bed6c3190d0aae90b29bcd594a1e64f4626a9c1e
Antti Mikkonen <antti.mikkonen@fmi.fi>
2024-12-18 16:19:17 +0200

    add interpolation experiment etst

---
+++ tests/run_interp_experiment.sh
@@ -0,0 +1,5 @@
+python interpolation_experiment.py nn asdfasdf
+python interpolation_experiment.py bb coarse
+python interpolation_experiment.py bb fine
+python interpolation_experiment.py bb extrafine
+python interpolation_experiment.py bb ultrafine
