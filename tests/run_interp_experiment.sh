32c93348e01bc1c18a2d172a5138d44821391e27
Antti Mikkonen <antti.mikkonen@fmi.fi>
2025-01-17 15:08:55 +0200

    edit license; remove tests

--- tests/run_interp_experiment.sh
+++
@@ -1,5 +0,0 @@
-python interpolation_experiment.py nn asdfasdf
-python interpolation_experiment.py bb coarse
-python interpolation_experiment.py bb fine
-python interpolation_experiment.py bb extrafine
-python interpolation_experiment.py bb ultrafine
